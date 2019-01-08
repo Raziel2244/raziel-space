c96b64b8f7671ae5c15efbbfdb2701d440be3227
Elliot <ethomas283@gmail.com>
2019-01-08 13:39:38 +0000

    Fixed FontAwesome Icons and changed colour to green

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { faDeviantart, faDiscord, faGit, faGithub, faInstagram, faMastodon, faPlaystation, faReddit, faSpotify, faSteam, faTelegram, faTumblrSquare, faTwitch, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-home',
@@ -7,19 +6,5 @@ import { faDeviantart, faDiscord, faGit, faGithub, faInstagram, faMastodon, faPl
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  faDeviantart = faDeviantart;
-  faDiscord = faDiscord;
-  faGit = faGit;
-  faGithub = faGithub;
-  faInstagram = faInstagram;
-  faMastodon = faMastodon;
-  faPlaystation = faPlaystation;
-  faReddit = faReddit;
-  faSpotify = faSpotify;
-  faSteam = faSteam;
-  faTelegram = faTelegram;
-  faTumblrSquare = faTumblrSquare;
-  faTwitch = faTwitch;
-  faTwitter = faTwitter;
-  faYoutube = faYoutube;
+  
 }
