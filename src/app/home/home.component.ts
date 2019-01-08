4018aadf4eaef74f1c6aec22249ebf7ea64941ed
Elliot <ethomas283@gmail.com>
2019-01-08 13:14:40 +0000

    Added profile image and various links

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { faDeviantart, faDiscord, faGit, faGithub, faInstagram, faMastodon, faPlaystation, faReddit, faSpotify, faSteam, faTelegram, faTumblrSquare, faTwitch, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class HomeComponent {
+  faDeviantart = faDeviantart;
+  faDiscord = faDiscord;
+  faGit = faGit;
+  faGithub = faGithub;
+  faInstagram = faInstagram;
+  faMastodon = faMastodon;
+  faPlaystation = faPlaystation;
+  faReddit = faReddit;
+  faSpotify = faSpotify;
+  faSteam = faSteam;
+  faTelegram = faTelegram;
+  faTumblrSquare = faTumblrSquare;
+  faTwitch = faTwitch;
+  faTwitter = faTwitter;
+  faYoutube = faYoutube;
 }
